49569ba1a6d14c2d970f8e7ba287a85f5481aeac
Sebastian Elm <sebastianelm@Sebastians-MacBook-Pro.local>
2017-05-03 23:54:32 +0200

    Anal2Digg.py

---
+++ recCOM.py
@@ -0,0 +1,24 @@
+# Server
+# !/user/bin/env python
+
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 20 # Normally 1024, but we want fast response
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.blind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print 'Connection addres: ', addr
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print "received data: ", data
+    conn.send(data)  # echo
+conn.close()
+
+
+
